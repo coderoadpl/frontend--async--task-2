832bf3986222d148ec10adb80cdde01f3420275f
Mateusz Choma <kontakt@coderoad.pl>
2020-10-18 16:02:41 +0200

    Simple event listener added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const button = document.querySelector('button')
+
+button.addEventListener(
+    'click',
+    () => {
+        setTimeout(() => {
+            console.log('You clicked the button')
+        }, 2000)
+    }
+)
+
 console.log('Hi!')
+
+setTimeout(() => {
+    console.log('Click the button!')
+}, 5000)
+
+console.log('Welcome to CodeRoad!')
